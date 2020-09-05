a772bccc398701c68a6ac4b150499ebcd5f64e9e
zxl19 <59674656+zxl19@users.noreply.github.com>
2020-09-05 17:56:31 +0800

    Update copyFile.m

--- Synchronization/copyFile.m
+++ Synchronization/copyFile.m
@@ -5,15 +5,15 @@ path_pcd_sync = "./pcd_sync/";
 [~, ~, ~] = mkdir(path_pcd_sync);
 [m, ~] = size(timestamp_img_sync);
 for i = 1 : m
-    img = strcat(path, strcat("img/", strcat(num2str(timestamp_img_sync(i, 1), '%6f'), ".jpg")));
+    img = strcat(path, strcat("img/", strcat(num2str(timestamp_img_sync(i, 1), '%.6f'), ".jpg")));
     [status_1, message_1, messageId_1] = copyfile(img, path_img_sync, 'f');
     if status_1 == 0
-        fprintf("Image      %s not found!\n", strcat(num2str(timestamp_img_sync(i, 1), 16), ".jpg"));
+        fprintf("Image      %s not found!\n", strcat(num2str(timestamp_img_sync(i, 1), '%.6f'), ".jpg"));
     end
-    pcd = strcat(path, strcat("pcd/", strcat(num2str(timestamp_pcd_sync(i, 1), '%6f'), strcat("000", ".pcd"))));
+    pcd = strcat(path, strcat("pcd/", strcat(num2str(timestamp_pcd_sync(i, 1), '%.6f'), strcat("000", ".pcd"))));
     [status_2, message_2, messageId_2] = copyfile(pcd, path_pcd_sync, 'f');
     if status_2 == 0
-        fprintf("Pointcloud %s not found!\n", strcat(num2str(timestamp_pcd_sync(i, 1), 16), strcat("000", ".pcd")));
+        fprintf("Pointcloud %s not found!\n", strcat(num2str(timestamp_pcd_sync(i, 1), '%.6f'), strcat("000", ".pcd")));
     end
 end
 end
