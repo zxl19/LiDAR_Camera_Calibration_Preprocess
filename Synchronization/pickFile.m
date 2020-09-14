f23a400eab867818b4de1ffcab3fe50058e608aa
zxl19 <59674656+zxl19@users.noreply.github.com>
2020-09-14 20:05:21 +0800

    Delete pickFile.m

--- Synchronization/pickFile.m
+++
@@ -1,8 +0,0 @@
-function pickFile(oldpath, newpath, filename, suffix)
-mkdir(newpath)
-[m, ~] = size(filename);
-for i = 1 : m
-    copyfile(strcat(oldpath, strcat(num2str(filename(i)), suffix)), ...
-        newpath);
-end
-end
